35eec273dab271c5d92071a5448ca3db3152d6c4
rizqydermawanai-png <rizqydermawanai@gmail.com>
2025-10-12 17:15:26 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ['lucide-react'], // Required for ESM packages
-};
-
-module.exports = nextConfig;
